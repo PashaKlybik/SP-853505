2b29cc67487ecacaf334ba4ddb82f3a14dceedab
NataliaMozhako <natalka.nachertalka@gmail.com>
2020-12-20 23:13:47 +0300

    Mozhako Natalia Labs 1-4

---
+++ LR1(6)/win1/framework.h
@@ -0,0 +1,11 @@
+﻿// Включаемый файл для стандартных системных включаемых файлов для конкретного проекта
+
+#pragma once
+
+#include "targetver.h"
+#define WIN32_LEAN_AND_MEAN  
+#include <windows.h>
+#include <stdlib.h>
+#include <malloc.h>
+#include <memory.h>
+#include <tchar.h>
